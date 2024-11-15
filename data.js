3e9ec50b4191e78332aff31cd05ac89027ddc2b4
tehnocipher <167606447+tehnocipher@users.noreply.github.com>
2024-11-15 16:57:11 +0100

    Add files via upload

--- data.js
+++ data.js
@@ -156,104 +156,104 @@ DATA = {
     desc : `Supreme analytics tool, swiss army knife of data analysis and data visualization`,
     power : true,
   },
-  program_30 : {
+  program_31 : {
     name : 'PAPER',
     link : 'https://fb.watch/u1KvRWfrBm/',
     desc : `Graphic editor, image creator, idea sketching tool, presentations tool`,
     power : true,
   },
-  program_31 : {
+  program_32 : {
     name : 'PCELAR',
     link : 'https://www.linkedin.com/pulse/p%2525C4%25258Delar-program-za-vo%2525C4%252591enje-evidencije-ko%2525C5%2525A1nica-kao-i-istih-petar/?trackingId=j6TMeHjDhJAIi2DqTrcJKQ%3D%3D',
     desc : `Beekeping made easy with hive organization digital tool`,
   },
-  program_32 : {
+  program_33 : {
     name : 'PIANO',
     link : '',
     desc : `Play Piano simulator`,
   },
-  program_33 : {
+  program_34 : {
     name : 'PINBALL',
     link : 'https://www.linkedin.com/pulse/fliper-petar-sladojevi%25C4%2587/?trackingId=dfDy1whNHdU%2FYq4ouk%2BJKQ%3D%3D',
     desc : `Legendary Pinball machine now in digital version`,
   },
-  program_34 : {
+  program_35 : {
     name : 'PONG',
     link : '',
     desc : `Pong Game`,
   },
-  program_35 : {
+  program_36 : {
     name : 'PREGLED',
     link : '',
     desc : `Simple image viewer`,
   },
-  program_36 : {
+  program_37 : {
     name : 'RIDER',
     link : '',
     desc : `Bike Racing game`,
   },
-  program_37 : {
+  program_38 : {
     name : 'ROS',
     link : 'https://fb.watch/u1K_fRiUbu/',
     desc : `Ready Operational System [JS-PHP-HTML-CSS] + fileSystem `,
     power : true,
   },
-  program_38 : {
+  program_39 : {
     name : 'SRP',
     link : '',
     desc : `SEO toolkit `,
   },
-  program_39 : {
+  program_40 : {
     name : 'SUDOKU',
     link : '',
     desc : `SUDOKU game`,
   },
-  program_40 : {
+  program_41 : {
     name : 'SUPER_BLOB',
     link : '',
     desc : `Jumping blob, the adventure game`,
   },
-  program_41 : {
+  program_42 : {
     name : 'SURF',
     link : '',
     desc : `Web browser [JS]`,
   },
-  program_42 : {
+  program_43 : {
     name : 'TABELA',
     link : '',
     desc : `SQL meets excel + table data viewer [JS]`,
   },
-  program_43 : {
+  program_44 : {
     name : 'TETRIS',
     link : 'https://fb.watch/u1K-P2Rfte/',
     desc : `Tetris, with traditional and new unique shapes`,
   },
-  program_44 : {
+  program_45 : {
     name : 'TRIMER',
     link : '',
     desc : `Trim and play audio, create slow tunes`,
   },
-  program_45 : {
+  program_46 : {
     name : 'VID',
     link : '',
     desc : `Video player [JS]`,
   },
-  program_46 : {
+  program_47 : {
     name : 'WHACK-A-JAR',
     link : 'https://www.facebook.com/reel/1530997947629427',
     desc : `Product business strategy gamification. [Advanced marketing]`,
   },
-  program_47 : {
+  program_48 : {
     name : 'WRITER',
     link : '',
     desc : `Simple text editor [JS]`,
   },
-  program_48 : {
+  program_49 : {
     name : 'X_O',
     link : '',
     desc : `Legendary X/O game`,
   },
-  program_49 : {
+  program_50 : {
     name : 'ZADACI',
     link : '',
     desc : `Reminder / To-Do [JS]`,
