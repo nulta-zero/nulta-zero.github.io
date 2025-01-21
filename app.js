8c71ff0d38962641c2653a5ad4e2c9827abd4379
nulta-zero <167606447+nulta-zero@users.noreply.github.com>
2025-01-21 10:20:14 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const doc = document,
 
 const $$ = {
     vars : {
-      availables : ['CALCULATOR', 'VIEWPORT', 'SOUNDPORT'],
+      availables : ['CALCULATOR', 'VIEWPORT', 'SOUNDPORT', 'MNEMONIC'],
       active_program : '',
       abc   : ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
     },
