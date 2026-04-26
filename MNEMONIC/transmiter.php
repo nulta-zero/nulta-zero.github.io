df6f0e3fb0ccf07b425dd164c1ef81c2d820959b
nulta-zero <167606447+nulta-zero@users.noreply.github.com>
2026-04-26 11:10:40 +0200

    Delete MNEMONIC/transmiter.php

--- MNEMONIC/transmiter.php
+++
@@ -1,6 +0,0 @@
-<?php
-header("Content-Type: application/json");
-$v = json_decode(file_get_contents("php://input" ) );     //stripslashes(
-$txt = json_encode($v, JSON_PRETTY_PRINT);
-file_put_contents('list.json', $txt);
-?>
