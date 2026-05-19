303bf08aaa85b2c106173cde701ab7a587c10a79
nulta-zero <167606447+nulta-zero@users.noreply.github.com>
2026-05-19 10:41:59 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const doc = document,
 
 const $$ = {
     vars : {
-      availables : ['CALCULATOR', 'VIEWPORT', 'SOUNDPORT', 'MNEMONIC'],
+      availables : ['CALCULATOR', 'VIEWPORT', 'SOUNDPORT', 'FEMY'],
       active_program : '',
       abc   : ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
     },
@@ -74,7 +74,7 @@ const $$ = {
 
           if(DATA[i].break){
              tr.classList.add('segment-break');
-             td2.innerText = '-- ' + DATA[i].desc + ' --'; 
+             td2.innerText = '-- ' + DATA[i].desc + ' --';
           }
 
           if(DATA[i].power){
